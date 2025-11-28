629373babe67f4cfe2ce9043c7981c475403bbd1
AzharMehmood4 <azharzharmehmoodm@gmail.com>
2025-11-28 20:48:44 +0500

    changes

---
+++ Js Foundation/part1/operations.js
@@ -0,0 +1,28 @@
+let score = 102
+let bonus = 25
+
+let totalScore = score + bonus
+
+
+let add = 3 + 2
+let sub = 3 - 2
+let mult = 3 * 2
+let dev = 3 / 2
+let remind = 3 % 2
+let expo = 3 ** 2
+
+let myscore = 102;
+myscore++;
+++myscore;
+
+let credits = 56;
+credits--;
+
+
+// compare operation
+
+let num1 = 2
+let num2 = 3
+let num3 = 6
+
+console.log(num1 == num2)
